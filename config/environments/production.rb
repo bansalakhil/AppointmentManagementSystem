b3242e7acd2034bc03c5cf209481dce8f59c7732
shruti2791 <shruti@vinsol.com>
2014-08-28 19:17:56 +0530

    AppointmentManagementSystem - Add smtp setting to config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,14 @@ Rails.application.configure do
 
   # Default url set for devise authentication
   config.action_mailer.default_url_options = {host: 'vinsol.com'}
+
+  config.action_mailer.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 end
